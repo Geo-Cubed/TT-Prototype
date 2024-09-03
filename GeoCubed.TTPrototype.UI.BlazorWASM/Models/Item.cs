0b8457500142a9ca1ae57c131004d7946dc5487e
George <44912087+Geo-Cubed@users.noreply.github.com>
2024-09-03 20:55:32 +0100

    Basic demo sorted

---
+++ GeoCubed.TTPrototype.UI.BlazorWASM/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace GeoCubed.TTPrototype.UI.BlazorWASM.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public bool Disabled { get; set; } = false;
+}
---
+++ GeoCubed.TTPrototype.UI.BlazorWASM/Models/Step.cs
@@ -0,0 +1,28 @@
+﻿namespace GeoCubed.TTPrototype.UI.BlazorWASM.Models;
+
+public class Step
+{
+    private readonly Item _event;
+    private readonly long _count;
+
+    public Step(Item stepEvent, long count)
+    {
+        this._event = stepEvent;
+        this._count = count;
+    }
+
+    public double Square()
+    {
+        return this._event.Id * this._event.Id;
+    }
+
+    public double Root()
+    {
+        return Math.Sqrt(this._event.Id);
+    }
+
+    public long OngoingCount()
+    {
+        return this._count + this._event.Id;
+    }
+}
